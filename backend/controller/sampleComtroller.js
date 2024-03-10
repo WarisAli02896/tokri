b78481afc458c0f08c337afbad6b5b4e187aa10d
GhulamSughraSoomro <118999870+gsughra@users.noreply.github.com>
2024-03-10 15:35:55 +0500

    test changes

--- backend/controller/sampleComtroller.js
+++ backend/controller/sampleComtroller.js
@@ -1,6 +1,7 @@
 
 
 exports.sampleFunction = (req, res) => {
+   console.log("hello");
     try{
         require("../models/auth/users");
         return res.status(200).json({
@@ -12,6 +13,7 @@ exports.sampleFunction = (req, res) => {
         return res.status(404).json({
             data:{
                 message: `${error}`
+                
             }
         })
     }
