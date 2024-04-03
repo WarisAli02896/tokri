8497f7940a1f7e4c437a3f72d4be614306d9d3f1
GhulamSughraSoomro <118999870+gsughra@users.noreply.github.com>
2024-04-03 23:52:57 +0500

    update pass and profile for customer

--- backend/route/customer/auth.js
+++ backend/route/customer/auth.js
@@ -2,12 +2,15 @@ const express = require("express");
 const { register_User } = require("../../controller/customer/auth/register");
 const { verify_user } = require("../../controller/auth/authorization");
 const { login } = require("../../controller/customer/auth/login");
+const { verifyAccessToken } = require("../../middleware/jsontoken");
+const { update_user } = require("../../controller/customer/auth/update");
+const { update_password } = require("../../controller/shopKeeper/auth/update");
 const router = express.Router();
 
-router.put('/user/update', verifyAccessToken, update_user);
+router.put("/user/update", verifyAccessToken, update_user);
 router.post("/user/register", register_User);
 router.get("/user/verify", verify_user);
 router.post("/user/login", login);
-router.put('/user/changepassword',verifyAccessToken,update_password);
+router.put("/user/changepassword", verifyAccessToken, update_password);
 
 module.exports = router;
